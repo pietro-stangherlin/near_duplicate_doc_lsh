4962055d37eda8436f2233a12359c4ec6402b795
pietro-stangherlin <pietrostangherlin@gmail.com>
2024-02-16 20:44:28 +0100

    In functions, in the shingling functions removed the option to not hash the just created shingles due to resource optimization for large inputs; slides updated about signatures creation

--- Project/tests/test_shingles.py
+++ Project/tests/test_shingles.py
@@ -4,45 +4,23 @@ def Simple_Hash(object):
     return hash(object) % 30
 
 # ---------------- Array Shingles ----------------#
-def Test_TextToShinglesArray_yesHash():
+def Test_TextToShinglesArray():
     w1 = "abcdefg"
-
-    t1 = funs.TextToShinglesArray(w1, 3, Simple_Hash, do_hash= True)
-
-    print(t1)
-
-def Test_TextToShinglesArray_noHash():
-    w1 = "abcdefg"
-
-    t1 = funs.TextToShinglesArray(w1, 3, Simple_Hash, do_hash= False)
-
+    t1 = funs.TextToShinglesArray(w1, 3, Simple_Hash)
     print(t1)
 
 # ------------ Set Shingles ---------------# 
-def Test_TextToShinglesSet_yesHash():
+def Test_TextToShinglesSet():
     w1 = "abcdefg"
-
-    t1 = funs.TextToShinglesSet(w1, 3, Simple_Hash, do_hash= True)
-
+    t1 = funs.TextToShinglesSet(w1, 3, Simple_Hash)
     print(t1)
-
-def Test_TextToShinglesSet_noHash():
-    w1 = "abcdefg"
-
-    t1 = funs.TextToShinglesSet(w1, 3, Simple_Hash, do_hash= False)
-
-    print(t1)
-
 
 # Warning: this script has to be executed 
 # from the (external) project directory as 
-# python -m tests.test_shingles.py  
+# python -m tests.test_shingles 
 
 if __name__ == "__main__":
     # -------- Array Shingles ------------#
-    Test_TextToShinglesArray_noHash() # ok
-    Test_TextToShinglesArray_yesHash() # ok
-
+    Test_TextToShinglesArray() # ok
     # -------- Set Shingles --------------#
-    Test_TextToShinglesSet_noHash() # ok
-    Test_TextToShinglesSet_yesHash() # ok
+    Test_TextToShinglesSet() # ok
