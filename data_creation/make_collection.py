7cbecacb1d57cdcd820326cf0d2c1ddf1c55d1d2
pietro-stangherlin <pietrostangherlin@gmail.com>
2024-02-23 15:37:32 +0100

    finished writing make_collection.py, but it needs to be tested

--- data_creation/make_collection.py
+++ data_creation/make_collection.py
@@ -1,15 +1,13 @@
 import json
 import random
-import re 
 import randomNoise as rn
 from typing import Callable
-
 
 # given a dataset like
 # {"id" = "...", "content": "...", "id2" : int}
 
-def EditTextOCR(text : str, 
-             error_params: list) -> str:
+def EditTextOCR(text : str,
+                error_params: list) -> str:
     '''Given some text return a modified version of it.
     (simulating OCR errors)
     
@@ -26,39 +24,64 @@ def EditTextOCR(text : str,
     
     Returns: 
         - edited text
+        
+    *NOTES: this function could be generalized accepting a list of functions
+    along with a list of list of their parameters, for now we keep it simple
     '''
     text = rn.OcrTransposition(text, error_params[0])
     text = rn.TransposeChars(text, error_params[1])
     
     return rn.SimulateOcrErrors(text, error_params[2])
 
-# To fix adding random noise functions
-# and their parameters
-# maybe it's better to return an iterator
-def EditJsonLineOCR(line: str,
-                 id2_last_index: int,
-                 error_params_list: list) -> list:
-    '''Edit line according to some criteria.
-    Return dictionary with modified lines and updated id2_index
+
+def EditDictOCR(dictionary: dict,
+                id_int_unique_last_index: int, # id2 last value
+                id_int_unique_field_name: str, # id2
+                id_int_link_field_name: str, # id3
+                content_field_name: str,
+                error_params_list: list,
+                edit_text_function: Callable,
+                ) -> iter:
+    '''Edit dictionary according to some criteria, produces an iterator of dictionaries made this way.
+    
+    Assuming there are k elements in error_params_list ->
+    the iterator apply 
     
     Args: 
-        - line: line to be edited
-        - id2_last_index: last numerical index used for documents
+        - dictionary: dictionary to be edited
+        - id_int_unique_last_index: last numerical index used for documents' id2
+        - id_int_unique_field_name: name of id2 field
+        - id_int_link_field_name: name of the new id field where the original
+                                doc id_int_unique is written
+        - content_field_name: name of the content (str) to be edited
         - error_params_list: list of list of parameters, 
                             the oder of elements in each sublist should 
                             match that in EditTextOCR function.
                             (example [[0.1, 0.5, 0], [0.3, 0.1, 0.8]])
+        - edit_text_function: function to edit the text inside the dictionary["contend_field_name"]
+                            it is assumed this functions parameters are:
+                            (text_to_be_edited, list_of_other_functions_params)
     
-    Returns
+    Yield:
+        - edited dictionary
+    
+    NOTES: The indexing of edited dictionary works only with our way of creating the new ids,
+    which assumes the maximum id of the collection is known
+    so each new id is made by one increment of the maximum
+    (this way we are sure each document has its unique id)
     '''
-    baseline = json.loads(line)
     
-    for change in error_params_list:
-         
-        
+    original_id_unique = dictionary[id_int_unique_field_name]
+    original_content = dictionary[content_field_name]
     
-    return line
-
+    # new numerical id2
+    next_id_int = id_int_unique_last_index + 1
+    
+    for error_list in error_params_list:
+        yield {id_int_unique_field_name: next_id_int,
+               id_int_link_field_name: original_id_unique,
+               content_field_name: edit_text_function(original_content, error_list)}
+        next_id_int += 1
 
 
 # robust2
@@ -67,10 +90,15 @@ def WriteRandomLines(file_in: str,
                      file_out_collection: str,
                      file_out_index: str,
                      n_random_lines: int,
-                     edit_line_fun : Callable,
+                     edit_dict_fun : Callable,
+                     id_int_unique_field_name: str,
+                     id_int_link_field_name: str,
+                     edit_text_function: Callable,
+                     content_field_name: str,
+                     error_params_list: list,
                      write_original_lines: bool = True,
-                     n_lines_in_file: int = 528155,
-                     id2_last_index: int =  528154):
+                     n_lines_in_file: int = 528155, # robust docs number
+                     id_int_unique_last_index: int =  528154): # robust docs number - 1
     '''Write a fixed number of lines from a file at random.
     
     Updates id2 for newly created documents and adds id3,
@@ -78,11 +106,22 @@ def WriteRandomLines(file_in: str,
     Args:
         - file_in: name of input file
         - file_out_collection: name of output file
-        - file_out_index: name of dictionary like file: {id_new: id_original, ...} 
+        - file_out_index: name index like file: 
+                        id_int_unique_1,id_int__unique_original_1\nid_int_unique_2,id_int_unique_original_2
         - n_random_lines: number of lines to write
-        - edit_line_fun : functions editing the line
+        - edit_line_fun: functions editing the dictionary corresponding to the json line
+        - id_int_unique_field_name: name of id unique id made by integers
+        - id_int_link_field_name: name of new id which get value None for original documents,
+                                for edited documents is used the value of id_int_unique of the
+                                document from which they are derived
+        - edit_text_function: functions actually used to edit the text
+        - content_field_name: name of field where the actual text to edit is
+        - error_params_list: list of lists of parameters: each sublist is the second
+                            parameter of edit_text_function
         - write_original_lines: write also the file_in lines (default True)
         - n_lines_in_file: number of lines in input file, if known
+        - id_int_unique_last_index: maximum int index in the collection,
+                                    needed to make new unique indexes by incrementing it
     
     Note: if n_lines_in_file is not given the program
     iterates over all lines to count them.
@@ -103,16 +142,33 @@ def WriteRandomLines(file_in: str,
         
         
         line_index = 0
+        id_int_unique_last_index += 1
         
         # we created the new dataset with id2 as
         # the line number in the file
         
         for line in fin:
+            original_line_dict = json.loads(line.strip())
+            
             if write_original_lines:
-                
-                fout.write(line)
+                original_line_dict[id_int_link_field_name] = None
+                file_out_collection.write(f"{original_line_dict}\n") # check if its json like, maybe json.dump is better
                 
             if line_index in edit_indexes:
-                
+                for edited in edit_dict_fun(original_line_dict,
+                                            id_int_unique_last_index, # id2 last value
+                                            id_int_unique_field_name, # id2
+                                            id_int_link_field_name, # id3
+                                            content_field_name,
+                                            error_params_list,
+                                            edit_text_function):
+                    
+                    file_out_collection.write(f"{edited}\n")
+                    
+                    # write the indexed in the index file
+                    file_out_index.write(f"{id_int_unique_last_index},{original_line_dict[id_int_unique_field_name]}")
+                    id_int_unique_last_index += 1
+                    
+                    
                 
             line_index += 1
