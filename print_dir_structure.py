3a89fe529efb65c2cb49f4cf2e3415ffe53d076b
pietro-stangherlin <pietrostangherlin@gmail.com>
2024-02-25 14:50:14 +0100

    added requirements.txt file and updated print_dir_structure.py so it ignores the .git folder and also add graphical indentation bars like | |--

--- print_dir_structure.py
+++ print_dir_structure.py
@@ -1,15 +1,15 @@
 import os
 
-# Print the current directory structure
-
 def list_files(startpath):
     for root, dirs, files in os.walk(startpath):
+        if '.git' in dirs:
+            dirs.remove('.git')  # don't visit .git directories
         level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
+        indent = '|   ' * (level - 1) + '|-- ' if level > 0 else ''
         print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
+        subindent = '|   ' * level + '|-- '
         for f in files:
             print('{}{}'.format(subindent, f))
 
-# Use the current working directory as the starting path
-list_files(os.getcwd())
+if __name__ == "__main__":
+    list_files(os.getcwd())
