40ab3ea69a73d6eb1b465b20c5f6856d21b5c5bc
pietro-stangherlin <pietro.stangherlin@gmail.com>
2024-02-27 11:12:54 +0100

    defined a simple 32 bit hash function used for permutations and a function to generate it's constant in a uniform random way

--- Project/src/hash.py
+++ Project/src/hash.py
@@ -3,6 +3,20 @@ import hashlib
 import numpy as np
 
 # ---------- Shingle Hash --------------
+# -- Unsigned 32 bit hash Murmur --
+def MurmUns32Hash(input_string):
+    '''Compute 32 unsigned int hash (Murmur Hash).
+
+    Reference: https://pypi.org/project/mmh3/
+
+    Args: 
+        - input_string: string to be hashed
+    
+    Returns:
+        - 32 bit unsigned integer hash 
+    '''
+    return mmh3.hash(input_string, signed = False)
+
 # -- Unsigned 64 bit hash Murmur --
 def MurmUns64Hash(input_string):
     '''Compute 64 unsigned int hash (Murmur Hash).
@@ -20,7 +34,6 @@ def MurmUns64Hash(input_string):
     return mmh3.hash64(input_string, signed = False)[0]
 
 # -- Unsigned 64 bit hash from hashlib --
-
 def SHA256Uns64Hash(input_string):
     '''Compute 64 unsigned int hash (hashlib.sha256).
 
@@ -38,9 +51,97 @@ def SHA256Uns64Hash(input_string):
     return int_hash
 
 # ---------- Signature permutation hash functions ---------------
-# Universal Hashing
-# add 32 bit hashing from datasketch
-# add 64 bit hashing from MIDST progject LSH subdirectories
+# hash functions serving as row permutations in MinHash:
+# MinHash requires the row permutations to be random
+# implying the set of hash functions used to simulate the permutations
+# should resemble a random extraction.
+
+# --- Universal Hashing families ---
+# In order to do this we use families of (near) universal hash functions.
+# Reference:
+# J.Lawrence Carter, Mark N. Wegman, "Universal classes of hash functions"
+# Journal of Computer and System Sciences, Volume 18, Issue 2, 1979, Pages 143-154.
+
+# -- Implementation --
+# The main idea is to have a hash function on the form (a*x + b) mod p
+# where x is the integer to be hashed,
+# a and b are constats extracted uniformly at random
+# and p is a prime.
+# NOTE: in practice the hash function could be a bit more complicated.
+
+# The implementations use some computational tricks.
+# Reference:
+# Mikkel Thorup and Yin Zhang, "Tabulation Based 5-Universal Hashing and Linear Probing",
+# 2010 Proceedings of the Workshop on Algorithm Engineering and Experiments (ALENEX), pages 62-76
+
+
+# -- 32 bit keys universal hashing --
+
+def HashMultShift32(x: np.int32,
+                    aux_params: np.array) -> np.int32:
+    """
+    Performs 2-universal hashing for a 32-bit key.
+
+    For this function aux_params should be a numpy array of two unsigned 64 bit integers
+
+    Reference:
+    Mikkel Thorup and Yin Zhang, "Tabulation Based 5-Universal Hashing and Linear Probing",
+    2010 Proceedings of the Workshop on Algorithm Engineering and Experiments (ALENEX), pages 62-76.
+    A.2  Multiplication-shift based hashing for32-bit keys.
+
+
+    Args:
+    - x: 32-bit int to be hashed.
+    - aux_params: numpy array of two unsigned 64 bit integers
+
+    Returns:
+    The result of the 2-universal hashing.
+    """
+    if len(aux_params) != 2:
+         print("Error: the aux_params should have only two elements")
+         return None
+
+    a = int(aux_params[0])
+    b = int(aux_params[1])
+
+    return (a * int(x) + b) >> 32
+
+
+def GenerateTwoUns64(num_tuples: int,
+                     seed: int) -> np.ndarray:
+        '''Compute array of num_tuples tuples of positive 64 bit integer.
+
+        The first tuple element is >= 1, the second >= 0.
+        Those will be used as parameters for hash functions parameters A and B
+        where hash(x, A, B) = some_function(A * x + B).
+
+        Args:
+        - num_tuples: number of tuples generated
+        - seed: used for reproducibility
+
+        Returns:
+            - numpy.ndarray of num_tuples tuples of two positive 64 bit integer
+        '''
+
+        
+        gen = np.random.RandomState(seed)
+
+        max_int = np.iinfo(np.uint64).max
+
+        return np.array(
+            [
+                (
+                    gen.randint(1, max_int, dtype = np.uint64),
+                    gen.randint(0, max_int, dtype = np.uint64),
+                )
+                for _ in range(num_tuples)
+            ],
+            dtype = np.uint64,
+        ).T
+
+
+
+# add 64 bit hashing from MIDST project LSH subdirectories
 
 
 
---
+++ Project/tests/test_hash.py
@@ -0,0 +1,34 @@
+from src import hash as hs
+import unittest
+import numpy as np
+
+
+INT_TYPE_32 = np.int32
+INT_TYPE_64 = np.int64
+INT_TYPE_U64 = np.uint64
+
+class TestHash(unittest.TestCase):
+
+    def test_GenerateTwoUns64(self):
+        num_tuples = 2
+        seed = 123
+        result = hs.GenerateTwoUns64(num_tuples, seed)
+        print(f'''GenerateTwoUns64 with num_tuples {num_tuples} and seed {seed}:
+               {result}''')
+        print(f"result type: {type(result)}")
+
+    def test_HashMultShift32(self):
+        x = 914636142
+        aux_params_1 = np.array((3624216819017203054, 4184670734919783523), dtype= INT_TYPE_U64).T
+        result = hs.HashMultShift32(x, aux_params_1)
+        print(f'''HashMultiShift32(x = {x}, aux_params = {aux_params_1}) = 
+              {result}''')
+
+
+
+# Warning: this script has to be executed 
+# from the (external) project directory as 
+# python -m unittest tests.test_hash
+
+if __name__ == "__main__":
+    unittest.main()
