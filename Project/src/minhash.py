5f5d8d9c115a8a2153c3893ccc31b96cf4b7b599
pietro-stangherlin <pietro.stangherlin@gmail.com>
2024-02-27 11:41:36 +0100

    added V2 version of ComputeHashValues and GenerateSignature, they accept as a parameter a general hash function and a list of lists of its constant parameters

--- Project/src/minhash.py
+++ Project/src/minhash.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Callable
 from BTrees._LOBTree import LOBTree
 
 # requires: family of hash functions
@@ -38,6 +39,13 @@ def SignatureSimilarity(sig1: np.array, sig2: np.array) -> float:
     
     return equals / l1
 
+
+# NOTE: there are two version of ComputeHashValues and GenerateSignature:
+# plain versions accept a list of hash_function as parameters
+# the V2 versions accept instead a general hash_function 
+# and a list of list of its parameters other than the argument to be hashed
+# V2 are preferred as they should work with other hash functions defined in the hash.py file
+
 def ComputeHashValues(integer: int,
                       hash_functions_list: list,
                       int_type = np.uint64) -> np.array:
@@ -61,6 +69,40 @@ def ComputeHashValues(integer: int,
 
     for i in range(k):
         values[i] = hash_functions_list[i](integer)
+    
+    return values
+
+def ComputeHashValuesV2(integer: int,
+                      hash_function: Callable,
+                      hash_params_list: list,
+                      int_type = np.uint64) -> np.array:
+    '''Compute array of hash values of shingle.
+    
+    It's used in the next functions to calculate the permutaions 
+    in the signature.
+
+    Example (but non limited to): hash(x, hash_params) = (x*hash_params[0] + hash_params[1]) % 5.
+    hash_params_list = [(3, 8), (7, 2)]
+
+    Args:
+        - integer: input to be hashed by all hash functions in the list.
+        - hash_function: hash functions object, accepts (x, hash_params)
+                        where x is the element to be hashed.
+        - hash_params_list: hash_params_list is a list of list, each one 
+                            containing a set of parameters used for the hash function.
+        - int_type: type of integer used in the numpy array.
+
+    
+    Returns:
+        hash_values_array: array of all the hash values
+
+    '''
+    k = len(hash_params_list)
+
+    values = np.empty(shape = k, dtype = int_type)
+
+    for i in range(k):
+        values[i] = hash_function(integer, hash_params_list[i])
     
     return values
 
@@ -139,6 +181,100 @@ def GenerateSignature(shingles: iter,
         else:
             temp_permuted_pos = ComputeHashValues(value,
                                         hash_functions_list,
+                                        int_type)
+            
+            
+        
+        
+        # confront and eventually update positions and signature
+        for hash_fun_index in range(num_hash_funs):
+            if temp_permuted_pos[hash_fun_index] < positions[hash_fun_index]:
+                positions[hash_fun_index] = temp_permuted_pos[hash_fun_index]
+                signature[hash_fun_index] = value
+
+    
+    return signature
+
+def GenerateSignatureV2(shingles: iter,
+                      hash_function: Callable,
+                      hash_params_list: list,
+                      use_permutations_dict: bool = False,
+                      permutations_dict: dict = None,
+                      int_type = np.uint64) -> np.array:
+    '''Generate signature from shingles array.
+
+        Let k be the number of hash functions elements the hash_params_list.
+        The function works on two arrays of length k: one is the signature
+        and it's inizialized with all elements set to the
+        first shingles_array element, the other
+        contains the position of the signature elements
+        determined by each hash function (permutation).
+        Conditionally on each hash function (each element of hash_params_list), for each shingles element
+        check its permuted position, if smaller than the position stored 
+        in the position array -> update that position with it and, in the signature array,
+        substitute the corresponding shingle with the current one. 
+
+        Implementation details.
+        If se_permutations_dict is True:
+            For each shingles element check its presence as a key
+            in hash_permutations_dict:
+            if present use those values, if not computes the k hashes and
+            updates hash_permutations_dict adding the array
+            to it with the shingle as a key.
+        Else (default) don't check for the dictionary and just compute each permutation
+    
+    Args:
+        - shingles: iterable object (list, set, numpy.array...) containing hashed shingles.
+        - hash_function: hash functions object, accepts (x, hash_params)
+                        where x is the element to be hashed.
+        - hash_params_list: hash_params_list is a list of list, each one 
+                            containing a set of parameters used for the hash function.
+        - use_permutations_dict: Use or not a dictionary to store
+                                    all the previous permutations (see implementation details).
+        - permutations_dict: dictionary where 
+                                    key = shingle value
+                                    value = np.array of k values (int) where
+                                    each value is the result of the j-th
+                                    hash function on key.
+        - int_type: type of integer used in the numpy array.
+
+    Returns:
+        signature_vector: np.array of k values (int) where
+                                    each value is the result of the j-th
+                                    hash function on shingles_array.
+    '''
+
+
+    num_hash_funs = len(hash_params_list)
+    
+    # allocate signature matrix
+    signature = np.zeros(shape = num_hash_funs,
+                        dtype = int_type)
+ 
+    # allocate permuted positions array
+    # fill it with the maximum int_type integer
+    # so it's surely greater or equal than the hashed values
+    # (assuming the hash functions generate smaller integers than max(int_type))
+    positions = np.full(shape = num_hash_funs,
+                        fill_value = np.iinfo(int_type).max,
+                        dtype = int_type)
+
+    for value in shingles:
+        
+        if use_permutations_dict:
+            if value in permutations_dict:
+                temp_permuted_pos = permutations_dict[value]
+            else:
+                temp_permuted_pos = ComputeHashValuesV2(value,
+                                        hash_function,
+                                        hash_params_list,
+                                        int_type)
+                permutations_dict[value] = temp_permuted_pos
+        
+        else:
+            temp_permuted_pos = ComputeHashValuesV2(value,
+                                        hash_function,
+                                        hash_params_list,
                                         int_type)
             
             
