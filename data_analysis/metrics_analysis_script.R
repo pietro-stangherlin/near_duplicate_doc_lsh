c735225c95438e4832b9aa04c2cd8fbec6a45a1f
pietro-stangherlin <pietrostangherlin@gmail.com>
2025-07-14 12:19:45 +0200

    fixed  R plotting function

--- data_analysis/metrics_analysis_script.R
+++ data_analysis/metrics_analysis_script.R
@@ -4,13 +4,23 @@ rm(list = ls())
 dir_list_all <- list.dirs(path = "../../data_near_duplicate/arxiv/lsh_results")
 dir_list = dir_list_all[2:length(dir_list_all)]
 
+
+
 # Constants ----------------- ----------------------------
 
 SHARED_BUCK_METRIC_NAME <- "metrics_shared_buckets_number.csv"
 SIGN_METRIC_NAME <- "metrics_signature_similarity.csv"
 
+SIGN_SIM_NAME = "signature_similarity"
+SHARED_BUCK_NAME = "shared_buckets_number"
+
 PCH_PRECISION = 16
 PCH_RECALL = 8
+PLOT_TYPE = "b"
+
+PRECISION_NAME = "precision"
+RECALL_NAME = "recall"
+
 
 
 # Parameters sets --------------------------------------
@@ -28,7 +38,7 @@ NOISE_QUANT <- c("no", "small", "mid") # small noise 2%, mid 5%
 DUPLICATE_PREFIX <- "per_"
 DUPLICATES_PERCENT = c(1, 5, 10, 25)
 
-SIGL_PREFIX <- "sig_"
+SIGL_PREFIX <- "sigl_"
 SIGL <- c(100, 200)
 
 NBA_PREFIX <- "nba_"
@@ -50,22 +60,14 @@ PREFIX_LIST <- setNames(
     NBU_NAME)
 )
 
-
-
-
-# compare one parameter fixing all the others
-
-
-# Signature Length ----------------------------------------------
-
-# Noise quantity ------------------------------------------------
-
-# Duplicates percentage -----------------------------------------
-
-# Number of bands -----------------------------------------------
-
-# Number of buckets ---------------------------------------------
-
+EMPTY_PARAMS_LIST <- setNames(
+  list(NA, NA, NA, NA, NA),
+  c(NOISE_QUANT_NAME,
+    DUPLICATES_PERCENT_NAME,
+    SIGL_NAME,
+    NBA_NAME,
+    NBU_NAME)
+)
 
 # Plotting function ------------
 
@@ -76,6 +78,7 @@ PREFIX_LIST <- setNames(
 # and vary just one
 
 #' @param params_list (list): list of type ()
+#' @param prefix_list (list)
 
 PlotSimMetricsVSPrecRecOneParam <- function(params_list,
                                             prefix_list,
@@ -90,7 +93,7 @@ PlotSimMetricsVSPrecRecOneParam <- function(params_list,
   
   all_indexes <- 1:length(params_list)
   
-  index_not_fixed_param <- which(params_list > 1)
+  index_not_fixed_param <- which(length(params_list) > 1)
   
   if(length(index_not_fixed_param) > 1){
     print("Error: more than one parameter has more than one value, leaving")
@@ -98,20 +101,28 @@ PlotSimMetricsVSPrecRecOneParam <- function(params_list,
   }
   
   fixed_params_indexes <- setdiff(all_indexes, index_not_fixed_param)
+  fixed_params_names <- names(params_list)[fixed_params_indexes]
   
+  # Fixed params folders name subset
+  fixed_params_values <- rep(NA, length(fixed_params_indexes))
   text_to_select = rep(NA, length(fixed_params_indexes))
   
-  for (i in fixed_params_indexes){
-    par_name = names(params_list)[i]
-    text_to_select[i] = paste0(prefix_list[[par_name]], params_list[[par_name]],
+  for (i in 1:length(fixed_params_indexes)){
+    actual_index <- fixed_params_indexes[i]
+    par_name = names(params_list)[actual_index]
+    fixed_params_values[i] <- params_list[[par_name]]
+    text_to_select[i] = paste0(prefix_list[[par_name]],
+                               params_list[[par_name]],
                                collapse = "")
   }
   
-  pattern <- paste0("(?=.*", text_to_select, ")", collapse = "")
+  pattern <- paste0("(?=.*", text_to_select, "(_|$))", collapse = "")
   
-  selected_dirs <- my_dir_list[grep(pattern,my_dir_list,
+  selected_dirs <- my_dir_list[grep(pattern,
+                                    my_dir_list,
                                     perl = TRUE)]
   
+  # Not Fixed param sub folders conditioned on the previuos found
   not_fixed_par_name = names(params_list)[index_not_fixed_param]
   
   # first value: reference plot
@@ -120,7 +131,8 @@ PlotSimMetricsVSPrecRecOneParam <- function(params_list,
                           params_list[[not_fixed_par_name]][1],
                           collapse = "")
   
-  used_dir <- my_dir_list[grep(text_to_select,selected_dirs)]
+  used_dir <- selected_dirs[grep(text_to_select,selected_dirs)]
+  
   
   metrics_df <- read.csv(paste0(c(used_dir, my_sim_name),
                                 collapse = "/"))
@@ -132,159 +144,98 @@ PlotSimMetricsVSPrecRecOneParam <- function(params_list,
        ylim = my_ylim,
        xlab = my_xlab,
        ylab = "metric",
-       pch = PCH_PRECISION)
+       pch = PCH_PRECISION,
+       type = PLOT_TYPE,
+       main = not_fixed_par_name)
   
   points(metrics_df[,x_axis_var_name],
          metrics_df[,recall_var_name],
          pch = PCH_RECALL, 
-         col = 1)
+         col = 1,
+         type = PLOT_TYPE)
   
   
   # other values: points on the reference plot
   
-  for(i in params_list[[index_not_fixed_param]][2:]){
+  color_index = 1
+  for(val in params_list[[index_not_fixed_param]][-1]){
+    color_index = color_index + 1
     
     text_to_select = paste0(prefix_list[[not_fixed_par_name]], val,
                                collapse = "")
     
     used_dir <- my_dir_list[grep(text_to_select,selected_dirs)]
     
-    metrics_df <- read.csv(paste0(c(used_dir, my_metrics_name),
+    metrics_df <- read.csv(paste0(c(used_dir, my_sim_name),
                                      collapse = "/"))
     
-    
-
-    
-    points(metrics_df_20[,x_axis_var_name],
-           metrics_df_20[,precision_var_name],
-           col = COL_nba_20,
-           pch = PCH_PRECISION)
-    
-    points(metrics_df_20[,x_axis_var_name],
-           metrics_df_20[,recall_var_name],
-           pch = PCH_RECALL,
-           col = COL_nba_20)
-    
-    legend("bottomleft",
-           legend = c("precision", "recall"),
-           pch = c(PCH_PRECISION, PCH_RECALL),
-           bty = "n")
-    
-    legend("bottomright",
-           legend = c("10 bands", "20 bands"),
-           col = c(COL_nba_10, COL_nba_20),
-           bty = "n",
-           lty = 1,
-           lwd = 2)
-    
-    
-  }
-  
-}
-
-
-
-PlotTwoBandsVSPrecisionREcall <- function(my_xlab,
-                                          my_xlim,
-                                          my_dir_list,
-                                          my_metrics_name,
-                                          x_axis_var_name,
-                                          precision_var_name,
-                                          recall_var_name){
-  
-  
-  for (nbuck in nbu){
-    for(siglen in sigl){
-      temp_pattern <- paste0(c(nbuck,"_sgn_shl_9_sigl_", siglen), collapse = "")
-      
-      temp_dirs <- my_dir_list[grep(temp_pattern,my_dir_list)]
-      
-      temp_nba_10 <- temp_dirs[grep("nba_10",temp_dirs)]
-      
-      metrics_df_10 <- read.csv(paste0(c(temp_nba_10, my_metrics_name),
-                                       collapse = "/"))
-      
-      temp_nba_20 <- temp_dirs[grep("nba_20",temp_dirs)]
-      
-      metrics_df_20 <- read.csv(paste0(c(temp_nba_20, my_metrics_name),
-                                       collapse = "/"))
-      
-      plot(metrics_df_10[,x_axis_var_name],
-           metrics_df_10[,precision_var_name],
-           col = COL_nba_10,
-           xlim = my_xlim,
-           ylim = c(0.4, 1),
-           xlab = my_xlab,
-           ylab = "metric",
+    points(metrics_df[,x_axis_var_name],
+           metrics_df[,precision_var_name],
+           col = color_index,
            pch = PCH_PRECISION,
-           main = paste0(c("N bucket: ", nbuck,
-                           "; Signature len : ", siglen), collapse = ""))
-      
-      points(metrics_df_10[,x_axis_var_name],
-             metrics_df_10[,recall_var_name],
-             pch = PCH_RECALL, 
-             col = COL_nba_10)
-      
-      points(metrics_df_20[,x_axis_var_name],
-             metrics_df_20[,precision_var_name],
-             col = COL_nba_20,
-             pch = PCH_PRECISION)
-      
-      points(metrics_df_20[,x_axis_var_name],
-             metrics_df_20[,recall_var_name],
-             pch = PCH_RECALL,
-             col = COL_nba_20)
-      
-      legend("bottomleft",
-             legend = c("precision", "recall"),
-             pch = c(PCH_PRECISION, PCH_RECALL),
-             bty = "n")
-      
-      legend("bottomright",
-             legend = c("10 bands", "20 bands"),
-             col = c(COL_nba_10, COL_nba_20),
-             bty = "n",
-             lty = 1,
-             lwd = 2)
-      
-      
-    }
+           type = PLOT_TYPE)
+    
+    points(metrics_df[,x_axis_var_name],
+           metrics_df[,recall_var_name],
+           pch = PCH_RECALL,
+           col = color_index,
+           type = PLOT_TYPE)
+   
   }
+  
+  legend("topleft",
+         legend = paste(fixed_params_names, ": ", fixed_params_values),
+         bty = "n")
+  
+  legend("bottomright",
+         legend = c(PRECISION_NAME, RECALL_NAME),
+         pch = c(PCH_PRECISION, PCH_RECALL),
+         bty = "n")
+  
+  # TO DO: LEGEND
+  
+  legend("bottomleft",
+         legend = paste(not_fixed_par_name, ": ", params_list[[index_not_fixed_param]]),
+         col = 1:color_index,
+         bty = "n",
+         lty = 1,
+         lwd = 2)
+  
 }
 
 
-# no noise ----------------------------
-dir_list_no_noise <- dir_list[grep("no_noise", dir_list)]
-
-# 10k ----------------------------------
-dir_list_10k <- dir_list[grep("_10k", dir_list)]
-
-# no noise 10k -------------------------------
-dir_list_no_noise_10k <- intersect(dir_list_no_noise,
-                                   dir_list_10k)
-
-par(mfrow = c(3,3))
-
-COL_nba_10 = "black"
-COL_nba_20 = "red"
+# compare one parameter fixing all the others
 
 
+# Noise quantity ------------------------------------------------
 
-PlotTwoBandsVSPrecisionREcall(my_xlab = "signature similarity",
-                              my_xlim = c(0.4, 1),
-                              my_dir_list = dir_list_no_noise_10k,
-                              my_metrics_name = SIGN_METRIC_NAME,
-                              x_axis_var_name = "signature_similarity",
-                              precision_var_name = "precision",
-                              recall_var_name = "recall")
+temp_param_list = EMPTY_PARAMS_LIST
+temp_param_list[[NOISE_QUANT_NAME]] = NOISE_QUANT
+temp_param_list[[DUPLICATES_PERCENT_NAME]] = DUPLICATES_PERCENT[1]
+temp_param_list[[SIGL_NAME]] = SIGL[1]
+temp_param_list[[NBA_NAME]] = NBA[1]
+temp_param_list[[NBU_NAME]] = NBU[2]
 
-PlotTwoBandsVSPrecisionREcall(my_xlab = "shared bucket number",
-                              my_xlim = c(5, 20),
-                              my_dir_list = dir_list_no_noise_10k,
-                              my_metrics_name = SHARED_BUCK_METRIC_NAME,
-                              x_axis_var_name = "shared_buckets_number",
-                              precision_var_name = "precision",
-                              recall_var_name = "recall")
+PlotSimMetricsVSPrecRecOneParam(params_list = temp_param_list,
+                                prefix_list = PREFIX_LIST,
+                                my_sim_name = SIGN_METRIC_NAME,
+                                x_axis_var_name = SIGN_SIM_NAME,
+                                precision_var_name = PRECISION_NAME,
+                                recall_var_name = RECALL_NAME,
+                                my_dir_list = dir_list,
+                                my_xlim = c(0.4, 1),
+                                my_ylim = c(0.4, 1),
+                                my_xlab = SIGN_SIM_NAME)
+
+# Duplicates percentage -----------------------------------------
+
+# Signature Length ----------------------------------------------
+
+# Number of bands -----------------------------------------------
+
+# Number of buckets ---------------------------------------------
+
+
 
 
 
