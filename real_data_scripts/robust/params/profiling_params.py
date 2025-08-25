3d7355e13a4e822891da067299bbe8ecb127a53e
pietro-stangherlin <pietrostangherlin@gmail.com>
2025-08-25 17:28:59 +0200

    updated profiling data

--- real_data_scripts/robust/params/profiling_params.py
+++ real_data_scripts/robust/params/profiling_params.py
@@ -1,9 +1,9 @@
 from near_duplicate_doc_lsh.project.src import minhash as mh
 
-N_BANDS = 20
+N_BANDS = 20 
 TIMES_BUCKET = 25
 
-DB_PATH = "data_near_duplicate\\robust\\signatures_db_duplicates\\sgn_shl_9_sigl_200_bit_uint32_mid_noise_per25\\signature_db"
+DB_PATH = "data_near_duplicate\\robust\\signatures_db_duplicates\\sgn_shl_9_sigl_100_bit_uint32_mid_noise_per25\\signature_db"
 
 OUT_FOLDER = "data_near_duplicate\\robust\\profiling\\"
 
